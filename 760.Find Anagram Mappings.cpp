714254855515b77c6eb60ad7a56159b645c88686
laoji525 <jixujia123@163.com>
2018-06-28 14:59:55 -0400

    Update 760.Find Anagram Mappings.cpp

--- 760.Find Anagram Mappings.cpp
+++ 760.Find Anagram Mappings.cpp
@@ -1,11 +1,15 @@
 class Solution {
 public:
     vector<int> anagramMappings(vector<int>& A, vector<int>& B) {
-        vector<int> temp;
-        for(int i = 0;i<A.size();i++){
+        vector<int> c;
+        int temp = 0;
+        for(int i = 0;i<A.size();i++)
             for(int j = 0;j<B.size();j++){
-                
+                if(A.at(i)==B.at(j)){
+                    c.push_back(j);
+                    break;
+                }
             }
-        }
+        return c;
     }
 };
