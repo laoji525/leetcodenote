67435795c3e18b51bd44fffec774184576857ffe
laoji525 <jixujia123@163.com>
2018-06-28 14:58:54 -0400

    Create 760.Find Anagram Mappings.cpp

---
+++ 760.Find Anagram Mappings.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> anagramMappings(vector<int>& A, vector<int>& B) {
+        vector<int> temp;
+        for(int i = 0;i<A.size();i++){
+            for(int j = 0;j<B.size();j++){
+                
+            }
+        }
+    }
+};
