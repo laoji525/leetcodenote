00c1fedbf1a68554c8ac2200612998da4aa6d89c
laoji525 <jixujia123@163.com>
2018-07-25 14:41:58 -0400

    Update 049.groupAnagrams.py

--- 049.groupAnagrams.py
+++ 049.groupAnagrams.py
@@ -1,1 +1,8 @@
-
+def gA(self, strs):
+  r = collections.defaultdict(list)
+  for s in strs:
+    count = [0] * 26
+    for c in s:
+      count[ord(c) - ord('a')] += 1
+    r[tuple(count)].append(s)
+  return r.values()
