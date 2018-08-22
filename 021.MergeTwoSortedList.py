e64e983b7d6c63fb78aeb5e4496222c15d82b06d
Xujia <jixujia123@163.com>
2018-08-22 20:42:55 +0800

    Create 021.MergeTwoSortedList.py

---
+++ 021.MergeTwoSortedList.py
@@ -0,0 +1,12 @@
+def mTSL(self, l1, l2):
+	dummy = curr = ListNode(0)
+	while l1 and l2:
+		if l1.val < l2.val:
+			curr.next = l1
+			l1 = l1.next
+		else:
+			curr.next = l2
+			l2 = l2.next
+		curr = curr.next
+	curr = l1 or l2
+	return dummy.next
